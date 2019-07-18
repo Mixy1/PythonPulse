a1c65a17a275188d95792ecdee2f51414243ce18
Mixy1 <Michael_debono@hotmail.com>
2019-07-18 13:19:45 +0200

    Update server.py

--- Messaging/server.py
+++ Messaging/server.py
@@ -3,6 +3,7 @@ from pynput.keyboard import Key, Controller
 from socket import *
 import pickle
 host = ""
+contacts = ["10.68.107.45","10.68.104.104"]
 port = 5000
 buf = 1024
 addr = (host, port)
@@ -14,8 +15,12 @@ print("Waiting to receive messages...")
 while True:
     (data, addr) = UDPSock.recvfrom(buf)
     data = data.decode()
-
-    print("Received message: " + str(data))
+    if(addr = contacts[0]):
+        addr = (contacts[1], port)
+        UDPSock.sendto(data, addr)
+    elif(addr = contacts[1]):
+        addr = (contacts[0], port)
+        UDPSock.sendto(data, port)
 
 
 
